6499bb157f900406b626a0ac092b2a3c446c96b0
Mayank Agarwal <mayank@playlyfe.com>
2015-09-27 01:22:58 +0530

    add react router history api. Basic login flow working...

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import App from './App';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 React.render(
-  <Router>
+  <Router history={createBrowserHistory()}>
+    <Route path="/team34" component={App} />
     <Route path="/" component={App} />
     <Route path="/login" component={Login} />
     <Route path="/dashboard" component={Dashboard} />
