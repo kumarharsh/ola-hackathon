55ff40e26d8f74a9c0ce6a34121823b4c3192aef
Peter John <peter@playlyfe.com>
2015-09-27 06:27:36 +0530

    feat(index): add set interval for tracking ride

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ import Driver from './components/Driver';
 import { Router, Route, Redirect } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import OlaApi from './Services/OlaApi';
+import fetch from 'isomorphic-fetch';
+
+let injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 function requireAuth(nextState, replaceState) {
   if (!OlaApi.isLoggedIn()) {
@@ -17,9 +26,16 @@ function requireAuth(nextState, replaceState) {
       client_id: 'YzcwZjI1MGEtZDExMC00Nzc3LTk4MTYtNTY4MTc4NTViMWNj',
       redirect_uri: 'http://localhost/team34&scope=profile%20booking&state=state123',
     });
-
     if (!OlaApi.isLoggedIn()) { // still not logged in redirect to login page
+      console.log('Please Login')
       replaceState({ nextPathname: nextState.location.pathname }, '/login');
+    } else {
+      console.log('Already Logged in')
+      setInterval(function() {
+        OlaApi.api('/v1/bookings/track_ride', 'GET', function(data) {
+          console.log("trar", data)
+        })
+      }, 5000)
     }
   }
 }
