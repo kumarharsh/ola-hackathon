d9b526d4c02f82e3dabb297448714699b62f7ccb
Kumar Harsh <khs@playlyfe.com>
2015-09-27 06:05:21 +0530

    feat(app): add a "Ride Now" button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Base from './Base';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import AppTheme from './theme';
 import RaisedButton from 'material-ui/lib/raised-button';
-import { AppBar, IconMenu, MenuItem, IconButton } from 'material-ui';
+import { AppBar, IconMenu, MenuItem, IconButton, FlatButton } from 'material-ui';
 import { Link } from 'react-router';
 import { OLA, others } from './colors';
 import Flame from './components/Streak/Flame';
@@ -47,22 +47,28 @@ export default class App extends Base {
     }
     return (
       <div>
-        <div className="user-profile" style={{backgroundColor: others.grey800, borderBottom:'2px solid ' + OLA, padding:'0 1em'}}>
-          <Link to={'/dashboard'} style={{display:'flex', height:'100%', color: others.white, textDecoration:'none'}}>
-            <div className="avatar" style={{borderRadius:'50%', width: user.avatar.medium.width, height: user.avatar.medium.height, overflow:'hidden', flexGrow:0, margin:'1em 0'}}>
-              <img src={user.avatar.medium.url} alt={user.name} style={{maxWidth:'100%', maxWidth:"100%"}}/>
-            </div>
-            <div className="user-details" style={{flex:'1 0 auto', padding:'1em 0.5em 1em', borderRight:'1px solid ' + others.grey900}}>
-              <h3 className="user-name" style={{margin: 0}}>{user.name}</h3>
-            </div>
-            <div className="user-points" style={{textAlign:'center', padding:'1em 1em 0.5em', flex:'1 0 auto', borderLeft:'1px solid ' + others.grey700}}>
-              <div style={{fontSize:'3rem',fontWeight:'bold'}}>{ola_points}</div>
-              <div style={{fontSize:'1rem'}}>OLA Points</div>
-            </div>
-            <div className="streak" style={{flexGrow:0, padding:'1em 0'}}>
-              <Flame streak={user.streak} width="50"/>
-            </div>
-          </Link>
+        <div style={{height:'100vh', display:'flex', flexDirection:'column'}}>
+          <div className="user-profile" style={{backgroundColor: others.grey800, borderBottom:'2px solid ' + OLA, padding:'0 1em'}}>
+            <Link to={'/dashboard'} style={{display:'flex', height:'100%', color: others.white, textDecoration:'none'}}>
+              <div className="avatar" style={{borderRadius:'50%', width: user.avatar.medium.width, height: user.avatar.medium.height, overflow:'hidden', flexGrow:0, margin:'1em 0'}}>
+                <img src={user.avatar.medium.url} alt={user.name} style={{maxWidth:'100%', maxWidth:"100%"}}/>
+              </div>
+              <div className="user-details" style={{flex:'1 0 auto', padding:'1em 0.5em 1em', borderRight:'1px solid ' + others.grey900}}>
+                <h3 className="user-name" style={{margin: 0}}>{user.name}</h3>
+              </div>
+              <div className="user-points" style={{textAlign:'center', padding:'1em 1em 0.5em', flex:'1 0 auto', borderLeft:'1px solid ' + others.grey700}}>
+                <div style={{fontSize:'3rem',fontWeight:'bold'}}>{ola_points}</div>
+                <div style={{fontSize:'1rem'}}>OLA Points</div>
+              </div>
+              <div className="streak" style={{flexGrow:0, padding:'1em 0'}}>
+                <Flame streak={user.streak} width="50"/>
+              </div>
+            </Link>
+          </div>
+          <div className="MAP" style={{flex:'1 0 300px'}}></div>
+          <div className="ride-status">
+            <FlatButton label="Ride Now" style={{backgroundColor:others.black, color:OLA, width:'100%', borderRadius:0, padding:'1em 0'}}/>
+          </div>
         </div>
       </div>
     );
